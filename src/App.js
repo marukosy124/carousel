9be09e31bf1daad0305fc35bfa9d1e1ddefa2ec8
viact <soniayeung12435@gmail.com>
2021-07-13 11:41:02 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React from "react";
+import Carousel from "./components/Carousel/Carousel";
+
+const App = () => {
+  return (
+    <div
+      style={{
+        maxWidth: 1200,
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginTop: 64,
+      }}
+    >
+      <Carousel show={2}>
+        <div>
+          <div style={{ padding: 8 }}>
+            <img
+              src="https://via.placeholder.com/300x300"
+              alt="placeholder"
+              style={{ width: "100%" }}
+            />
+          </div>
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <img
+              src="https://via.placeholder.com/300x300"
+              alt="placeholder"
+              style={{ width: "100%" }}
+            />
+          </div>
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <img
+              src="https://via.placeholder.com/300x300"
+              alt="placeholder"
+              style={{ width: "100%" }}
+            />
+          </div>
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <img
+              src="https://via.placeholder.com/300x300"
+              alt="placeholder"
+              style={{ width: "100%" }}
+            />
+          </div>
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <img
+              src="https://via.placeholder.com/300x300"
+              alt="placeholder"
+              style={{ width: "100%" }}
+            />
+          </div>
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <img
+              src="https://via.placeholder.com/300x300"
+              alt="placeholder"
+              style={{ width: "100%" }}
+            />
+          </div>
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <img
+              src="https://via.placeholder.com/300x300"
+              alt="placeholder"
+              style={{ width: "100%" }}
+            />
+          </div>
+        </div>
+        <div>
+          <div style={{ padding: 8 }}>
+            <img
+              src="https://via.placeholder.com/300x300"
+              alt="placeholder"
+              style={{ width: "100%" }}
+            />
+          </div>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,113 @@
+import React, { useEffect, useState, useRef } from "react";
+import "./carousel.css";
+
+const Carousel = (props) => {
+  const { children, show } = props;
+
+  const sliderRef = useRef(null);
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [length, setLength] = useState(children.length);
+
+  const [touchPosition, setTouchPosition] = useState(null);
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(0);
+
+  // Set the length to match current children from props
+  useEffect(() => {
+    setLength(children.length);
+  }, [children]);
+
+  const next = () => {
+    if (currentIndex < length - show) {
+      setCurrentIndex((prevState) => prevState + 1);
+    }
+  };
+
+  const prev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((prevState) => prevState - 1);
+    }
+  };
+
+  const handleTouchStart = (e) => {
+    const touchDown = e.touches[0].clientX;
+    setTouchPosition(touchDown);
+  };
+
+  const handleTouchMove = (e) => {
+    const touchDown = touchPosition;
+
+    if (touchDown === null) {
+      return;
+    }
+
+    const currentTouch = e.touches[0].clientX;
+    const diff = touchDown - currentTouch;
+
+    if (diff > 5) {
+      next();
+    }
+
+    if (diff < -5) {
+      prev();
+    }
+
+    setTouchPosition(null);
+  };
+
+  const handleMouseDown = (e) => {
+    setIsMouseDown(true);
+    setStartX(e.pageX - sliderRef.current.offsetLeft);
+    setScrollLeft(sliderRef.current.scrollLeft);
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isMouseDown) return;
+    e.preventDefault();
+    const x = e.pageX - sliderRef.current.offsetLeft;
+    const walk = (x - startX) * 3; //scroll-fast
+    sliderRef.current.scrollLeft = scrollLeft - walk;
+  };
+
+  return (
+    <div className="carousel-container">
+      <div className="carousel-wrapper">
+        {/* You can alwas change the content of the button to other things */}
+        {currentIndex > 0 && (
+          <button onClick={prev} className="left-arrow">
+            &lt;
+          </button>
+        )}
+        <div
+          className="carousel-content-wrapper"
+          ref={sliderRef}
+          onTouchStart={handleTouchStart}
+          onTouchMove={handleTouchMove}
+          onMouseDown={handleMouseDown}
+          onMouseLeave={() => setIsMouseDown(false)}
+          onMouseUp={() => setIsMouseDown(false)}
+          onMouseMove={handleMouseMove}
+        >
+          <div
+            className={`carousel-content show-${show}`}
+            style={{
+              transform: `translateX(-${currentIndex * (100 / show)}%)`,
+            }}
+          >
+            {children}
+          </div>
+        </div>
+        {/* You can alwas change the content of the button to other things */}
+        {currentIndex < length - show && (
+          <button onClick={next} className="right-arrow">
+            &gt;
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
