dcc0897136212f80da179bc5af7aff60cbfbf092
viact <soniayeung12435@gmail.com>
2021-07-27 18:27:40 +0800

    add radio button (not yet complete)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Carousel from './components/Carousel/Carousel';
+import './app.css';
 
 const renderImages = () => {
   const images = [];
   for (let i = 0; i < 8; i++) {
     images.push(
-      <div>
+      <div key={i}>
         <div style={{ padding: 8 }}>
           <img
             src="https://via.placeholder.com/300x300"
@@ -20,17 +21,41 @@ const renderImages = () => {
 };
 
 const App = () => {
+  const [itemsToShow, setItemsToShow] = useState(2);
+  // const [checkStatus, setCheckStatus] = useState({1: });
+
+  const renderRadioButtonss = () => {
+    const radioButtons = [];
+    for (let i = 1; i <= 4; i++) {
+      radioButtons.push(
+        <label className="checkbox-label" key={i}>
+          <input
+            type="radio"
+            name={i}
+            checked={false}
+            onChange={(e) => setItemsToShow(e.target.name)}
+          />
+          {i}
+        </label>
+      );
+    }
+    return radioButtons;
+  };
+
   return (
-    <div
-      style={{
-        maxWidth: 1200,
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        marginTop: 64,
-      }}
-    >
-      <Carousel show={3}>{renderImages()}</Carousel>
-    </div>
+    <>
+      <div
+        style={{
+          maxWidth: 1200,
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          marginTop: 64,
+        }}
+      >
+        <Carousel show={itemsToShow}>{renderImages()}</Carousel>
+      </div>
+      {renderRadioButtonss()}
+    </>
   );
 };
 
